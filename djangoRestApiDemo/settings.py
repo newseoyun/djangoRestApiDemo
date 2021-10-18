1a994f2b81ed01e39370977a9d8f9d62402164e9
seoyun <newseoyun@gmail.com>
2021-10-18 13:02:36 +0900

    runTest

--- djangoRestApiDemo/settings.py
+++ djangoRestApiDemo/settings.py
@@ -38,14 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'product'
 ]
 
-REST_FRAMEWORK = [
+REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAdminUser',
     ],
     'PAGE_SIZE': 10
-]
+}
 
 
 MIDDLEWARE = [
--- djangoRestApiDemo/urls.py
+++ djangoRestApiDemo/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from product import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'persons', views.PersonViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
